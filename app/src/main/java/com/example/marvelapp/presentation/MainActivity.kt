b7686a7a15a863430a1101a1f9452cb5bce5751a
anton <antoniojoseuchoa2023@gmail.com>
2023-01-21 16:41:15 -0300

    Commit implementando navegacao base

--- app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
@@ -2,11 +2,39 @@ package com.example.marvelapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.marvelapp.R
+import com.example.marvelapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+       val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+       navController = navHostFragment.navController
+       appBarConfiguration = AppBarConfiguration(
+           setOf(R.id.charactersFragment, R.id.favoritesFragment, R.id.aboutFragment)
+       )
+
+      binding.bottonNavigationView.setupWithNavController(navController)
+
+      binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+      // codigo opcional - caso precise tirar os botoes de voltar
+      navController.addOnDestinationChangedListener { _, destination, _ ->
+          val isTopLevelDestination = appBarConfiguration.topLevelDestinations.contains(destination.id)
+          if (!isTopLevelDestination) {
+                binding.toolbar.setNavigationIcon(R.drawable.ic_back)
+          }
+      }
     }
 }
